7a726976c7903fce31666eac272b7258940ab9e7
tosiq ahmed <m.tosiqshafiq@yahoo.com>
2021-03-27 14:06:43 +0500

    done working again

---
+++ WebApplication21/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication21.Models;
+
+namespace WebApplication21.Controllers
+{
+public  class HomeController : Controller
+    {
+        static List<Employee> obj = new List<Employee>();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost] //annotation
+        public ActionResult Index(Employee a)
+        {
+            obj.Add(a);
+            //ViewBag.id   =a.id;
+            //ViewBag.name = a.name;
+            //ViewBag.password = a.password;
+            //ViewBag.age = a.age;
+            ModelState.Clear();
+
+            return RedirectToAction("Select","Home");
+        }
+
+        public ActionResult Select()
+        {
+            return View(obj.ToList());
+        }
+    }
+}
---
+++ WebApplication21/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication21.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public int age { get; set; }
+
+    }
+}
